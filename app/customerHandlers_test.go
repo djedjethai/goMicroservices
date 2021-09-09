27a1e8e08bc661faa9fb30a3052ec7ade80e25a1
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-09-09 19:55:29 +0700

    costumerHandlers routes tested

--- app/customerHandlers_test.go
+++ app/customerHandlers_test.go
@@ -13,6 +13,7 @@ import (
 
 var router *mux.Router
 var mockService *service.MockCustomerService
+var ch customerHandlers
 
 func setup(t *testing.T) func() {
 	// Arrange
@@ -23,16 +24,58 @@ func setup(t *testing.T) func() {
 
 	// define customer handler
 	// inject the mock service
-	ch := customerHandlers{mockService}
+	ch = customerHandlers{mockService}
 
 	router = mux.NewRouter()
-	router.HandleFunc("/customers", ch.getAllCustomers)
+	// router.HandleFunc("/customers", ch.getAllCustomers)
 
 	// close the ctrl as cb func
 	// otherwise it will close the connection before the test finished
 	return func() {
 		router = nil
 		defer ctrl.Finish() // !!! important
+	}
+}
+
+func Test_should_return_error_message_with_status_code_500_for_selected_customer(t *testing.T) {
+	// Arrange
+	tearDown := setup(t)
+	defer tearDown()
+
+	// set an unexisting id
+	mockService.EXPECT().GetCustomer("2006").Return(nil, errs.NewInternalServerError("some err in db"))
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer)
+	request, _ := http.NewRequest(http.MethodGet, "/customers/2006", nil)
+
+	// Act
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	if recorder.Code != http.StatusInternalServerError {
+		t.Error("While testing 'getCustomer', statusCode should be 500")
+	}
+}
+
+func Test_should_return_selected_customers_with_status_code_200(t *testing.T) {
+	// Arrange
+	tearDown := setup(t)
+	defer tearDown()
+
+	dummyCustomer := &dto.CustomerResponse{Id: "1001", Name: "Jeje", City: "paris", ZipCode: "75000", DateOfBirth: "2000-1-1", Status: "1"}
+	mockService.EXPECT().GetCustomer("1001").Return(dummyCustomer, nil)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer)
+	request, _ := http.NewRequest(http.MethodGet, "/customers/1001", nil)
+
+	// Act
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	if recorder.Code != http.StatusOK {
+		t.Error("While testing 'getCustomer' the statusCode should be 200")
 	}
 }
 
@@ -48,6 +91,7 @@ func Test_should_return_customers_with_status_code_200(t *testing.T) {
 	mockService.EXPECT().GetAllCustomer("").Return(dummyCustomers, nil)
 
 	// make the http req
+	router.HandleFunc("/customers", ch.getAllCustomers)
 	request, _ := http.NewRequest(http.MethodGet, "/customers", nil)
 
 	// Act
@@ -67,6 +111,7 @@ func Test_should_return_error_message_with_status_code_500(t *testing.T) {
 	tearDown := setup(t)
 	defer tearDown()
 	mockService.EXPECT().GetAllCustomer("").Return(nil, errs.NewInternalServerError("some db errs"))
+	router.HandleFunc("/customers", ch.getAllCustomers)
 	request, _ := http.NewRequest(http.MethodGet, "/customers", nil)
 
 	// Act
